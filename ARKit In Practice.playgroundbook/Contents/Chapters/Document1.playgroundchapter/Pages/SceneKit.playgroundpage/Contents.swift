6c1089b091717b0eb3590055f3f9d10113e35f85
Niels van Hoorn <nvh@nvh.io>
2018-01-02 17:15:17 +0100

    Added playgroundbook and readme

---
+++ ARKit In Practice.playgroundbook/Contents/Chapters/Document1.playgroundchapter/Pages/SceneKit.playgroundpage/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+import ARKit
+
+class ARDelegate: NSObject, ARSCNViewDelegate {
+    @objc func didTapView(sender: UIGestureRecognizer) {
+    }
+}
+
+let arView = ARSCNView()
+arView.debugOptions = [.showPhysicsShapes]
+
+let delegate = ARDelegate()
+arView.delegate = delegate
+
+arView.scene = SCNScene()
+
+let config = ARWorldTrackingConfiguration()
+config.planeDetection = ARWorldTrackingConfiguration.PlaneDetection.horizontal
+arView.session.run(config, options: [])
+
+let gesture = UITapGestureRecognizer(target: delegate, action: #selector(ARDelegate.didTapView))
+arView.addGestureRecognizer(gesture)
+
+import PlaygroundSupport
+PlaygroundPage.current.liveView = arView
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# ARKit In Practice
+
+These are the resulting playgrounds of my talk at [Swift Paris]https://www.meetup.com/swiftparis/events/243703627/).
+
+To play around with it, airdrop the `ARKit In Practice.playgroundbook` to you iPad running iOS 11 with Swift Playgrounds installed and it will automatically open.
+
+Unfortunely Playground Books aren't compatible with the current version of XCode.
